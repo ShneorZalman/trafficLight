6ea151f6f9035c42e0844a780c33e96077a4e453
Zalman Zoteev <sz.zoteev@gmail.com>
2023-03-19 22:28:09 +0200

    update outlets

--- trafficLight/ViewController.swift
+++ trafficLight/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLabel: UIView!
+    @IBOutlet var yellowLabel: UIView!
+    @IBOutlet var greenLabel: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        redLabel.alpha = 0.3
+        yellowLabel.alpha = 0.3
+        greenLabel.alpha = 0.3
     }
-
-
+    @IBAction func startButtonDidTapped(_ sender: UIButton) {
+        sender.setTitle("Next", for: .normal)
+        redLabel.alpha = 1
+        if redLabel.alpha == 1 {
+            yellowLabel.alpha = 1
+            redLabel.alpha = 0.3
+            greenLabel.alpha = 0.3
+        }
+    }
 }
 
