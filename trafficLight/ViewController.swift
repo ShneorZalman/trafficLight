76e9ee89729fba68f6a0c174d21849579fbb2ec9
Zalman Zoteev <sz.zoteev@gmail.com>
2023-03-21 23:18:30 +0200

    final

--- trafficLight/ViewController.swift
+++ trafficLight/ViewController.swift
@@ -7,26 +7,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
     
     @IBOutlet var redLabel: UIView!
     @IBOutlet var yellowLabel: UIView!
     @IBOutlet var greenLabel: UIView!
-    var changeColour = "red"
+    private var changeColour = "red"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         redLabel.alpha = 0.3
         yellowLabel.alpha = 0.3
         greenLabel.alpha = 0.3
-        redLabel.layer.cornerRadius = 84
-        yellowLabel.layer.cornerRadius = 84
-        greenLabel.layer.cornerRadius = 84
+        redLabel.layer.cornerRadius = redLabel.frame.width / 2
+        yellowLabel.layer.cornerRadius = yellowLabel.frame.width / 2
+        greenLabel.layer.cornerRadius = greenLabel.frame.width / 2
     }
     @IBAction func startButtonDidTapped(_ sender: UIButton) {
-//        redLabel.alpha = 1
-        sender.setTitle("Next", for: .normal)
-        
+        if sender.currentTitle != "Next" {
+            sender.setTitle("Next", for: .normal)
+        }
         if changeColour == "red" {
             redLabel.alpha = 1
             greenLabel.alpha = 0.3
